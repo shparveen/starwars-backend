209206861e1fcd9ce9a050c5418d0495d33e3fab
Parveen Shaikh <shparmeen@gmail.com>
2020-01-30 18:58:19 +0530

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -21,7 +21,7 @@ class Api extends CI_Controller {
 	public function Planets($id = 0)
 	{
         $data = $this->ApiOperation->getPlanets($id);     
-		return $this->output->set_content_type('application/json')->set_status_header(400)->set_output(json_encode($data));
+		return $this->output->set_content_type('application/json')->set_status_header(200)->set_output(json_encode($data));
 	}
 	
 	/*
@@ -31,9 +31,11 @@ class Api extends CI_Controller {
     */
 	public function longestCrawl()
 	{   
+
 		$data = array();
 		$data = $this->ApiOperation->getLongestCrawl();
-		return $this->output->set_content_type('application/json')->set_status_header(400)->set_output(json_encode($data));
+		$this->output->set_header('Cache-Control: post-check=0, pre-check=0');
+		return $this->output->set_header('Access-Control-Allow-Origin: *')->set_content_type('application/json')->set_status_header(200)->set_output(json_encode($data));
 	}
 	
 	/*
@@ -44,7 +46,7 @@ class Api extends CI_Controller {
 	public function mostAppearCharacter(){
 		$data = array();
 		$data = $this->ApiOperation->getMostAppearCharacter();
-		return $this->output->set_content_type('application/json')->set_status_header(400)->set_output(json_encode($data));
+		return $this->output->set_header('Access-Control-Allow-Origin: *')->set_content_type('application/json')->set_status_header(200)->set_output(json_encode($data));
 	}	
 	
 	/*
@@ -55,7 +57,7 @@ class Api extends CI_Controller {
 	public function starWarsSpecies(){
 		$data = array();
 		$data = $this->ApiOperation->getStarWarSpecies();
-		return $this->output->set_content_type('application/json')->set_status_header(400)->set_output(json_encode($data));
+		return $this->output->set_header('Access-Control-Allow-Origin: *')->set_content_type('application/json')->set_status_header(200)->set_output(json_encode($data));
 	}	
 	
 	
@@ -72,19 +74,24 @@ class Api extends CI_Controller {
 			$i=0;
 			foreach($datas as $data){
 				
-				$resData[$i]['planet_detail'] = "Planet: ".$data->planet_name." - Pilots: (".$data->total_vehicle_pilots.")";
-				$resData[$i]['planet_pilots_detail'] = [];
+				$resData[$i]['planet_detail'] = "PLANETS: ".$data->planet_name." - Pilots: (".$data->total_vehicle_pilots.") ";
+			//	$resData[$i]['planet_pilots_detail'] = array();
 				$planet_pilots_species_datas = $this->ApiOperation->getPlanetPilotsDetail($data->planet_id);
 				
 				$j=0;
 				foreach($planet_pilots_species_datas as $planet_pilots_species_data){
-					$resData[$i]['planet_pilots_detail'][$j]['pilot_name'] = $planet_pilots_species_data->pilot_name;
-					$resData[$i]['planet_pilots_detail'][$j]['species_name'] = (empty($planet_pilots_species_data->species_id))?"":$planet_pilots_species_data->species_id;
+					$species_name = (empty($planet_pilots_species_data->species_id))?"species name not found":$planet_pilots_species_data->species_id;
+					$resData[$i]['planet_detail'] .= " ".$planet_pilots_species_data->pilot_name." - ".$species_name.", ";
+					//$resData[$i]['planet_pilots_detail'][$j]['pilot_name'] = $planet_pilots_species_data->pilot_name;
+					//$resData[$i]['planet_pilots_detail'][$j]['species_name'] = $species_name;
+					
 					$j++;
 				}
+				$resData[$i]['planet_detail'] = rtrim($resData[$i]['planet_detail']);
+				$resData[$i]['planet_detail'] = substr($resData[$i]['planet_detail'],0,strlen($resData[$i]['planet_detail'])-1);
 				$i++;
 			}			
 		}
-		return $this->output->set_content_type('application/json')->set_status_header(400)->set_output(json_encode($resData));
+		return $this->output->set_header('Access-Control-Allow-Origin: *')->set_content_type('application/json')->set_status_header(200)->set_output(json_encode($resData));
 	} 
 }
